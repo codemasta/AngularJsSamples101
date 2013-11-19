b4835af631d447ef29609f12eac853ab2565a8c4
codemasta <seun2talk@gmail.com>
2013-11-20 00:04:30 +0100

    Display Data in Tables and Creating new records
    Sample code for displaying data in tables and adding new records to the
    table

---
+++ App/Js/StudentController.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+myApp.controller('StudentController', function($scope) {
+
+    var studentsArray = []; // empty array
+
+    function student(name, sex, level) { // student class creation
+        this.name = name;
+        this.sex = sex;
+        this.level = level;
+    }
+
+    studentsArray.push(new student('Ade Yinka', 'Male', 500));
+    studentsArray.push(new student('Bankole Jude', 'Female', 200));
+    $scope.students = studentsArray;
+
+    $scope.Save = function() {
+        studentsArray.push(new student($scope.newStudent.name, $scope.newStudent.sex, $scope.newStudent.level));
+        $scope.newStudent.name = null;
+        $scope.newStudent.sex = null;
+        $scope.newStudent.level = null;
+    };
+});
